511db838b5eb2ec8bbc86e070c66e8ffc54790d5
Rob Hoelz <rob@hoelz.ro>
2011-10-10 20:44:01 -0500

    Finishing touches on Twitter auth

--- lib/Act/AuthMethod/Twitter.pm
+++ lib/Act/AuthMethod/Twitter.pm
@@ -21,20 +21,24 @@ sub new {
 
     return unless defined($consumer_key) && defined($consumer_secret);
 
-    my $self    = Act::AuthMethod::new($class);
-    unless($twitter) {
-        $twitter = Net::Twitter->new(
-            traits          => [qw/OAuth API::REST/],
-            consumer_key    => $consumer_key,
-            consumer_secret => $consumer_secret,
-        );
-    }
+    my $twitter = Net::Twitter->new(
+        traits          => [qw/OAuth API::REST/],
+        consumer_key    => $key,
+        consumer_secret => $secret,
+    );
+
+    my $self = Act::AuthMethod::new($class);
 
     my $callback_url       = make_abs_uri('auth_methods/twitter');
-    $self->{'twitter_url'} = $twitter->get_authorization_url(
+    $self->{'twitter_uri'} = $twitter->get_authorization_url(
         callback => $callback_url);
 
-    #$Request{r}->session->{'net_twitter'} = $twitter;
+    my $session = $Request{r}->session;
+
+    $session->{'twitter'} = {
+        token        => $twitter->request_token,
+        token_secret => $twitter->request_token_secret,
+    };
 
     return $self;
 }
@@ -42,8 +46,8 @@ sub new {
 sub render {
     my ( $self ) = @_;
 
-    #my $uri        = $self->{'twitter_url'};
-    my $uri        = 'http://localhost:5000';
+    my $uri = $self->{'twitter_uri'};
+
     my $login_text = localize('Login with Twitter');
 
     return <<HTML;
@@ -58,14 +62,51 @@ sub name {
 sub handle_postback {
     my ( $self, $req ) = @_;
 
-    my $twitter = $Request{r}->session->{'net_twitter'};
+    my $session  = $req->session;
+    my $verifier = $req->param('oauth_verifier');
 
-    use Data::Dumper::Concise;
-    print STDERR Dumper($req->env);
+    my $twitter = Net::Twitter->new(
+        traits => [qw/API::REST OAuth/],
+    );
+    $twitter->request_token($session->{'twitter'}{'request_token'});
+    $twitter->request_token_secret($session->{'twitter'}{'request_token_secret'});
+
+    delete $session->{'twitter'};
+
+    my ( $access_token, $access_token_secret, $user_id, $screen_name ) =
+        $twitter->request_access_token(verifier => $verifier);
+
+    my $dbh = $Request{'dbh'};
+    my $sth = $dbh->prepare(<<SQL);
+SELECT u.user_id FROM twitter_auths AS ta
+INNER JOIN users AS u
+ON ta.twitter_id = ?
+SQL
+
+    $session->{'auth_method_info'} = {
+        access_token        => $access_token,
+        access_token_secret => $access_token_secret
+        user_id             => $user_id,
+        screen_name         => $screen_name,
+    };
+    $sth->execute($user_id);
+
+    my ( $user_id ) = $sth->fetchrow_array;
+
+    return $user_id;
 }
 
 sub associate_with_user {
     my ( $self, $req, $user ) = @_;
+
+    my ( $token, $token_secret, $twitter_id ) =
+        @{$req->session->{'auth_method_info'}}{qw/access_token access_token_secret user_id/}
+
+    my $dbh = $Request{dbh};
+    $dbh->do(<<SQL, undef, $user->user_id, $twitter_id, $token, $token_secret);
+INSERT INTO twitter_auths (user_id, twitter_id, access_token,
+    access_token_secret) VALUES (?, ?, ?, ?)
+SQL
 }
 
 1;
